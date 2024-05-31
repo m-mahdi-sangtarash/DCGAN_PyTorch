2c17160f70d5ccb8ba4cffd3a02e1cf0e5e4141a
Mohammad Mahdi Sangtarash <m.mahdi.sangtarash@gmail.com>
2024-06-01 00:17:53 +0330

    DCGAN Model - Train and Run - Fix bugs

--- DCGAN-Model-PyTorch.py
+++ DCGAN-Model-PyTorch.py
@@ -9,7 +9,6 @@ import torchvision.utils as vutils
 import numpy as np
 import zipfile
 import matplotlib.animation as animation
-from IPython.display import HTML
 
 batch_size = 128
 z_dim = 100  # 100-dimensional noise vector as the input to generator
@@ -17,12 +16,11 @@ num_epoch = 3
 learning_rate = 0.0002  # Optimizer learning rate
 betal = 0.5  # Momentum value for Adam optimizer
 
-# with zipfile.ZipFile('img_align_celeba.zip') as zip_ref:
-#     zip_ref.extractall('./data/celeba')
+with zipfile.ZipFile('img_align_celeba.zip') as zip_ref:
+    zip_ref.extractall('./data/celeba')
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(device)
 root = './data/celeba'
 dataset = datasets.ImageFolder(root=root,
                                transform=transforms.Compose([
